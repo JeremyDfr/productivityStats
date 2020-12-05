7ab61986c3527a467fd109f856202ef7c9ef7bcc
jeremy <jeremy@webilo.fr>
2020-12-05 11:11:08 +0100

    Create range component

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,18 +11,48 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <range-component :range-title="energy.title" :class="energy.style"></range-component>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <range-component :range-title="concentration.title" :class="concentration.style"></range-component>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <range-component :range-title="motivation.title" :class="motivation.style"></range-component>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol} from '@ionic/vue';
+import RangeComponent from '@/components/RangeComponent.vue';
 
-export default  {
+export default {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonGrid, IonRow, IonCol, RangeComponent},
+  data: () => ({
+    energy: {
+      title: 'Energy',
+      style: 'energy-style'
+    },
+    concentration: {
+      title: 'Concentration',
+      style: 'concentration-style'
+    },
+    motivation: {
+      title: 'Motivation',
+      style: 'motivation-style'
+    },
+  })
 }
 </script>
